785855a45fdbc1d27ed8f86724f74eb71fdeb5a5
Julio Ramirez <juemrami@gmail.com>
2024-05-18 17:33:25 -0700

    minor: include Cata in "Tags.lua" invalid dungeon cleanup (juemrami/LFG-Bulletin-Board#30)

--- LFGBulletinBoard/Tags.lua
+++ LFGBulletinBoard/Tags.lua
@@ -584,25 +584,24 @@ GBB.dungeonSecondTags = {
 }
 
 -- Remove any unused dungeon tags based on game version
-if isClassicEra then
-	-- Get available dungeon tag keys up to current expansion
-	-- this includes raids/bgs/arenas/dungeons
-	local validDungeonKeys = GBB.GetSortedDungeonKeys()
-	local validGameVersionKeys = {}
-	for _, key in ipairs(validDungeonKeys) do
-		validGameVersionKeys[key] = true
-	end
-	for _, key in ipairs(GBB.Misc) do	
-		validGameVersionKeys[key] = true
-	end
-	-- iterate over all locales and `nil` out any entries for dungeons not valid for vanilla
-	for locale, dungeonTags in pairs(GBB.dungeonTagsLoc) do
-		for dungeonKey, _ in pairs(dungeonTags) do
-			if not (validGameVersionKeys[dungeonKey] 
-				or GBB.dungeonSecondTags[dungeonKey])
-			then
-				GBB.dungeonTagsLoc[locale][dungeonKey] = nil
-			end
+-- Get available dungeon tag keys up to current expansion
+-- this includes raids/bgs/arenas/dungeons
+local validDungeonKeys = GBB.GetSortedDungeonKeys()
+local validGameVersionKeys = {}
+for _, key in ipairs(validDungeonKeys) do
+	validGameVersionKeys[key] = true
+end
+for _, key in ipairs(GBB.Misc) do	
+	validGameVersionKeys[key] = true
+end
+
+-- iterate over all locales and `nil` out any entries for dungeons not in current client
+for locale, dungeonTags in pairs(GBB.dungeonTagsLoc) do
+	for dungeonKey, _ in pairs(dungeonTags) do
+		if not (validGameVersionKeys[dungeonKey] 
+			or GBB.dungeonSecondTags[dungeonKey])
+		then
+			GBB.dungeonTagsLoc[locale][dungeonKey] = nil
 		end
 	end
 end
